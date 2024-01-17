3835b1e7c6026d524b121f8414e012148a5e9666
TranBinh96 <binhvinhphong@gmail.com>
2024-01-17 16:28:02 +0700

    update categories

---
+++ shop_baby_service/src/main/java/com/project/shopbaby/ShopBabyServiceApplication.java
@@ -0,0 +1,13 @@
+package com.project.shopbaby;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopBabyServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopBabyServiceApplication.class, args);
+	}
+
+}
---
+++ shop_baby_service/src/test/java/com/project/shopbaby/ShopBabyServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.shopbaby;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopBabyServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
